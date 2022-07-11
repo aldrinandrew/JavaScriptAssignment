1cc787352dbe317cef5a14d0e32d21761a4de875
Aldrin Andrew <andrewjeromias@gmail.com>
2022-07-11 18:59:45 +0530

    Add JavaScript Array Assignment_2

--- Session 2_ Assignment/Arrays/Assignment_2.js
+++ Session 2_ Assignment/Arrays/Assignment_2.js
@@ -1,6 +1,20 @@
 /*
 
+[ 25 , 16 , 9 , 4 , 36 ]
+
 For the same array given above use filter() to extract the even numbers into another array. 
 And sort() the resultant array.
 
 */
+
+const arr = [ 25 , 16 , 9 , 4 , 36 ];
+const arr_even = arr.filter(evenNumber);
+
+
+function evenNumber(num) {
+    return num%2 == 0;
+
+}
+
+
+console.log(arr_even);
