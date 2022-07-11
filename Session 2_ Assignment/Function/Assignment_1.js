227d134f45a72f806e61a01232f0b6b7a2a89c60
Aldrin Andrew <andrewjeromias@gmail.com>
2022-07-11 18:17:35 +0530

    Add JavaScript Function Assignment files

---
+++ Session 2_ Assignment/Function/Assignment_1.js
@@ -0,0 +1,11 @@
+/*
+
+Write a function to find the greatest of 3 numbers and returns the greater value.Using  Arrow Function
+
+*/
+
+let myFunction = (a, b, c) => {
+    console.log(Math.max(a,b,c));
+}
+
+myFunction(5,10,2);
---
+++ Session 2_ Assignment/Function/Assignment_2.js
@@ -0,0 +1,12 @@
+/*
+
+Write a function to convert celsius to fahrenheit.
+
+*/
+
+function convertToF(celsius) {
+    farenheit = celsius * 9/5 + 32;
+    console.log(farenheit);
+  }
+  
+  convertToF(32);
