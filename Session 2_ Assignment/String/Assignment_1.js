c9b728d43b64f3e58dbf4871bd057fbb63995b48
Aldrin Andrew <andrewjeromias@gmail.com>
2022-07-11 17:46:30 +0530

    Add JavaScript Assignment on String

---
+++ Session 2_ Assignment/String/Assignment_1.js
@@ -0,0 +1,8 @@
+/*
+
+Combine “Key” and ”Value” using concat method, without space in between
+
+*/
+
+result = "Key".concat("Value");
+console.log(result);
---
+++ Session 2_ Assignment/String/Assignment_2.js
@@ -0,0 +1,10 @@
+/*
+
+Extract the “bring home” from “These are the things that I bring home”
+
+*/
+
+let str1 = "These are the things that I bring home";
+let str2 = str1.slice(28,38);
+
+console.log(str2);
